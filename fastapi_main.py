7f9ac8400a68899e2496f65a430190497d3c2b5e
anuragbisht12 <anuragbisht12@gmail.com>
2020-05-10 00:50:42 +0530

    Update fastapi_main.py

--- fastapi_main.py
+++ fastapi_main.py
@@ -2,8 +2,13 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
+import joblib,os
+
+yourmodel=open('LOCATION','rb')
+model=joblib.load(yourmodel)
 
 app = FastAPI()
+
 class Item(BaseModel):
     name: str
     price: float
@@ -21,9 +26,14 @@ def read_item(item_id: int, q: str = None):
 def create_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
+# ML function, you can customize as per your needs
+@app.get("/predict/{item_id}")
+def predict(item_id):
+    prediction=model.predict([item_id])
+    return {"item_id":item_id, "prediction":prediction}
 
 if __name__=='__main__':
     uvicorn.run(app,host='127.0.0.1',port=8000)
 
-#run using  
+#run server either calling the python file or using below command  
 # uvicorn fastapi_main:app --reload
